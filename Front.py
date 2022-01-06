16767955b062c483b895d46bfba03751233c5373
Jeff <duanhany@gmail.com>
2022-01-06 22:49:47 +0800

    some changes

--- Front.py
+++ Front.py
@@ -1,8 +1,42 @@
 from tkinter import *
 from tkinter import ttk
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
+from tkinter import messagebox
+
+class Interface(Frame):
+    def __init__(self, parent) -> None:
+        Frame.__init__(self, parent)   
+        self.parent = parent
+        self.UI()
+        self.callback()
+    
+    def UI(self):
+        self.parent.title("TEST")
+        self.parent.geometry("700x700")
+        style = ttk.Style()
+        style.theme_use("aqua")
+
+        
+
+        ttk.Label(self.parent, text = 'Input something :', font=('calibre',25, 'bold')).place(x=40,y=60)
+        ttk.Entry(self.parent, font=('calibre',25,'normal')).place(x=245,y=60)
+    
+    def callback(self, input):
+        if input.isdigit():
+            print(input)
+            return True
+                            
+        elif input is "":
+            print(input)
+            return True
+
+        else:
+            print(input)
+            return False
+
+
+
+    
+if __name__ == "__main__":
+    root = Tk()
+    app = Interface(root)
+    root.mainloop()
